bb88f1bf526faf8597475081cbecb8225c5a2449
Cheng Xiancheng <chengxc1990@outlook.com>
2018-03-15 11:36:16 +0800

    输出多页面

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,40 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main:'./src/index.js',
+        user:'./src/index.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
         new HtmlWebpackPlugin({
+            //html源文件路径
             template: path.resolve(__dirname, "./src/index.html"),
+            //编译之后的html文件名
+            filename:"main.html",
             hash:true,
             minify:{
                 //去除html中的空白字符
                 collapseWhitespace:true,
                 removeComments:true,
                 removeEmptyAttributes:true
-            }
+            },
+            chunks: ['main']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename:"user.html",
+            hash:true,
+            minify:{
+                //去除html中的空白字符
+                collapseWhitespace:true,
+                removeComments:true,
+                removeEmptyAttributes:true
+            },
+            chunks: ['user']
         }),
         new ExtractTextPlugin("style.css")
     ],
